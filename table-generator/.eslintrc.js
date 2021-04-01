576680df5e98a8a7ed69911ae70a57b9e16ca412
DrkNowak <dariusznowak194@gmail.com>
2021-04-01 10:10:17 +0200

    refactor: whole project

---
+++ table-generator/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/base',
+    'plugin:vue/recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+};
