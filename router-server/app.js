16dd0f966b4a474bea8877bb5edf39c4cae56546
DrkNowak <dariusznowak194@gmail.com>
2021-04-08 14:31:48 +0200

    fix: cors added

--- router-server/app.js
+++ router-server/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dataSummary from './dataSummary.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.listen(3000);
 
 app.get('/', (req, res) => {
