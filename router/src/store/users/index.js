55e13fc6f6b8a71360b4115f038cd60ba09801fa
DrkNowak <dariusznowak194@gmail.com>
2021-04-15 12:04:40 +0200

    feat: namespacing added

--- router/src/store/users/index.js
+++ router/src/store/users/index.js
@@ -3,4 +3,4 @@ import actions from './users.actions';
 import getters from './users.getters';
 import mutations from './users.mutations';
 
-export default { state, actions, getters, mutations };
+export default { namespaced: true, state, actions, getters, mutations };
