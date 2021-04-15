d7e9fc38be6b3ddd50c105470185f5b6eb231d41
DrkNowak <dariusznowak194@gmail.com>
2021-04-15 13:37:08 +0200

    refactor: try deleted

--- router/src/store/users/users.actions.js
+++ router/src/store/users/users.actions.js
@@ -2,10 +2,8 @@ import userService from '@/services/userService';
 
 export default {
   async fetchUsers({ commit }) {
-    try {
-      const { data = {} } = (await userService.getUsers()) || {};
+    const { data = {} } = (await userService.getUsers()) || {};
 
-      commit('SET_USER', data);
-    } catch (error) {}
+    commit('SET_USER', data);
   }
 };
