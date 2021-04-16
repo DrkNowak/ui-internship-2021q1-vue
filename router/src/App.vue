ddd164ca477f9f04a56b28c10b8812f6039ab8c1
DrkNowak <dariusznowak194@gmail.com>
2021-04-16 19:20:50 +0200

    feat: ag grid table

--- router/src/App.vue
+++ router/src/App.vue
@@ -21,7 +21,10 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import '../node_modules/ag-grid-community/dist/styles/ag-grid.css';
+@import '../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css';
+
 * {
   font-family: 'Squada One', cursive;
   margin: 0;
--- router/src/views/UserManagement.vue
+++ router/src/views/UserManagement.vue
@@ -1,15 +1,92 @@
 <template>
-  <div class="user-management" @click="showUsers"></div>
+  <div>
+    <AgGridVue
+      class="ag-theme-alpine grid"
+      :column-defs="columnDefs"
+      :row-data="rowData"
+      :grid-options="gridOptions"
+      :pagination="true"
+      :pagination-auto-page-size="true"
+      :suppress-auto-size="true"
+      :default-col-def="defaultColDef"
+    />
+  </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import { AgGridVue } from 'ag-grid-vue';
 
 export default {
   name: 'UserManagement',
 
+  components: {
+    AgGridVue
+  },
+
+  data() {
+    return {
+      columnDefs: [],
+      gridOptions: {}
+    };
+  },
+
   computed: {
-    ...mapGetters('users', { users: 'getUsers' })
+    ...mapGetters('users', { users: 'getUsers' }),
+
+    rowData() {
+      const rows = this.users.map((itemField) => ({
+        email: itemField?.email,
+        geography: itemField?.geos.map((geoItem) => geoItem?.name),
+        roles: itemField?.roleToAccess.map((roleItem) => roleItem?.role?.name),
+        teams: itemField?.teams.map((teamItem) => teamItem?.name)
+      }));
+
+      const countedRows = rows.map((row) => {
+        const countedRow = {};
+
+        Object.keys(row).forEach((field) => {
+          const isAmount =
+            typeof row[field] === 'object' && row[field].length > 1;
+
+          isAmount
+            ? (countedRow[field] = `${row[field].length} (${row[field]})`)
+            : (countedRow[field] = row[field]);
+        });
+
+        return countedRow;
+      });
+
+      return countedRows;
+    }
+  },
+
+  beforeMount() {
+    this.columnDefs = [
+      {
+        field: 'email',
+        tooltipField: 'email'
+      },
+      {
+        field: 'geography',
+        tooltipField: 'geography'
+      },
+      {
+        field: 'roles',
+        tooltipField: 'roles'
+      },
+      {
+        field: 'teams',
+        tooltipField: 'teams'
+      }
+    ];
+
+    this.defaultColDef = {
+      resizable: true,
+      sortable: true,
+      filter: true,
+      flex: 1
+    };
   },
 
   async mounted() {
@@ -27,9 +104,7 @@ export default {
 </script>
 
 <style scoped>
-.user-management {
-  width: 400px;
-  height: 400px;
-  background-color: red;
+.grid {
+  height: 600px;
 }
 </style>
