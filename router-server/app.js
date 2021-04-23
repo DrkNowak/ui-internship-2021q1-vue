8dcf69c2df511865ea3e6c768ccfccf54bdf55eb
DrkNowak <dariusznowak194@gmail.com>
2021-04-23 10:17:00 +0200

    feat: delete call added

--- router-server/app.js
+++ router-server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 
 const dataSummary = require('./dataSummary.json');
 const users = require('./users.json');
@@ -16,5 +17,23 @@ app.get('/', (req, res) =>{
 
 app.get('/users', (req, res) =>{
   setTimeout(() => res.json(users), 3);
+  
+})
+
+app.delete('/users:id', (req, res)=>{
+  const  id  = req.params
+  const desiredIndex = id.id.slice(1,id.id.length)
+  users.forEach((element,index) => {
+    
+    
+  if(element.id === parseInt(desiredIndex)) {
+  
+    users.splice(index,1);
+  }
+  });
+ 
+  fs.writeFileSync('users.json', JSON.stringify(users))
+  res.send('user deleted')
+  
   
 })
