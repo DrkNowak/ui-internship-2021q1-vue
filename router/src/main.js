17226971b57ae1d7c237f48c06aefcdb0d6b477f
DrkNowak <dariusznowak194@gmail.com>
2021-04-23 09:38:17 +0200

    feat: veautify added

--- router/src/main.js
+++ router/src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index';
 import apiCall from '@/mixins/apiCall';
+import outclick from '@/directives/OutClick';
+import vuetify from './plugins/vuetify';
 
 Vue.mixin(apiCall);
+Vue.directive('outclick', outclick);
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App)
 }).$mount('#app');
