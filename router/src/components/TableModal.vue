c19be12c498810b1b0fdf560548127a62b94c7ce
DrkNowak <dariusznowak194@gmail.com>
2021-04-30 14:19:37 +0200

    refactor: simplification and refactor

--- router/src/components/TableModal.vue
+++ router/src/components/TableModal.vue
@@ -1,78 +1,68 @@
 <template>
-  <div id="app">
-    <div class="text-center">
-      <v-dialog v-model="dialog" width="1000">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn dark v-bind="attrs" v-on="on"> add Role </v-btn>
-        </template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="1000">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn dark v-bind="attrs" v-on="on"> add Role </v-btn>
+      </template>
 
-        <v-card class="modal-dialog">
+      <v-card class="modal-dialog">
+        <v-btn icon dark color="#000" class="close-btn" @click="dialog = false">
+          <v-icon>mdi-close</v-icon></v-btn
+        >
+        <div class="form-header">
+          <h3>Add role & permissions</h3>
+        </div>
+
+        <div v-if="geos" class="card-container">
+          <v-select
+            v-model="newRole.geos"
+            :items="geos"
+            outlined
+            return-object
+            class="card-container__geos"
+            item-text="name"
+            item-value="id"
+            label="Geos"
+          ></v-select>
+          <v-text-field
+            v-model="newRole.name"
+            outlined
+            :rules="[rules.required]"
+            label="Role name"
+            class="card-container__role-name"
+          />
+        </div>
+
+        <v-container v-if="formData" fluid class="perm-container">
+          <div v-for="(perms, permName) in formData" :key="permName">
+            <h3 class="perm-container__headers">
+              {{ checkboxHeaders[permName] }}
+            </h3>
+            <v-checkbox
+              v-for="permission in perms"
+              :key="permission.name"
+              v-model="newRole[permName]"
+              :value="permission"
+              :label="`${permission.sapId} - ${permission.name}`"
+            ></v-checkbox>
+          </div>
+        </v-container>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn outlined rounded text @click="dialog = false"> Cancel </v-btn>
           <v-btn
-            icon
-            dark
-            color="#000"
-            class="close-btn"
-            @click="dialog = false"
+            outlined
+            rounded
+            text
+            :disabled="!isFulfilled"
+            @click="confirmForm"
           >
-            <v-icon>mdi-close</v-icon></v-btn
-          >
-          <div class="form-header">
-            <h3>Add role & permissions</h3>
-          </div>
-
-          <div v-if="geos" class="card-container">
-            <v-select
-              v-model="newRole.geos"
-              :items="geos"
-              outlined
-              return-object
-              class="card-container__geos"
-              item-text="name"
-              item-value="id"
-              label="Geos"
-            ></v-select>
-            <v-text-field
-              v-model="newRole.name"
-              outlined
-              :rules="[rules.required]"
-              label="Role name"
-              class="card-container__role-name"
-            />
-          </div>
-
-          <v-container v-if="formData" fluid class="perm-container">
-            <div v-for="(perms, permName) in formData" :key="permName">
-              <h3 class="perm-container__headers">
-                {{ checkboxHeaders[permName] }}
-              </h3>
-              <v-checkbox
-                v-for="permission in perms"
-                :key="permission.name"
-                v-model="newRole[permName]"
-                :value="permission"
-                :label="`${permission.sapId} - ${permission.name}`"
-              ></v-checkbox>
-            </div>
-          </v-container>
-
-          <v-card-actions>
-            <v-spacer />
-            <v-btn outlined rounded text @click="dialog = false">
-              Cancel
-            </v-btn>
-            <v-btn
-              outlined
-              rounded
-              text
-              :disabled="!isFulfilled"
-              @click="confirmForm"
-            >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </div>
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -131,7 +121,7 @@ export default {
     }
   },
 
-  async mounted() {
+  mounted() {
     this.fetchRoles();
   },
 
@@ -146,12 +136,15 @@ export default {
         businessTypes
       } = this.newRole;
 
+      const getIDs = (object) =>
+        object.map((objectItem) => ({ id: objectItem.id }));
+
       const desiredFormat = {
-        name: name,
+        name,
         geos: { id: geos.id },
-        orderTypes: orderTypes.map((order) => ({ id: order.id })),
-        salesOrganizations: salesOrganizations.map((org) => ({ id: org.id })),
-        businessTypes: businessTypes.map((type) => ({ id: type.id }))
+        orderTypes: getIDs(orderTypes),
+        salesOrganizations: getIDs(salesOrganizations),
+        businessTypes: getIDs(businessTypes)
       };
 
       console.log(desiredFormat);
