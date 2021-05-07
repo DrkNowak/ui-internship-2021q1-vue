be59c4bfe667a6f997d1048f9f2378a9e325e6d6
DrkNowak <dariusznowak194@gmail.com>
2021-05-07 15:52:58 +0200

    feat: task resolved

---
+++ vue-d3/src/components/Chart.vue
@@ -0,0 +1,171 @@
+<template>
+  <div>
+    <div id="svgcontainer"></div>
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+import chartData from '@/data/chartData';
+
+export default {
+  data() {
+    return {
+      chartProps: {
+        width: 1000,
+        height: 400,
+        verticalPadding: 50,
+        horizontalPadding: 30,
+        labelWidth: 40,
+        labelHeight: 50,
+        axisLeftLabelPosition: {
+          x: 30,
+          y: -12
+        },
+        unitDivider: 1000
+      },
+      chartData: {}
+    };
+  },
+
+  beforeMount() {
+    this.chartData = chartData;
+  },
+
+  mounted() {
+    this.drawChart(this.chartProps);
+  },
+
+  methods: {
+    drawChart(chartProps) {
+      const data = this.chartData;
+
+      const generateRandomColor = () =>
+        `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+      const svg = d3
+        .select('#svgcontainer')
+        .append('svg')
+        .attr('width', chartProps.width)
+        .attr('height', chartProps.height);
+
+      const xScale = d3
+        .scaleBand()
+        .rangeRound([
+          chartProps.horizontalPadding,
+          chartProps.width - chartProps.horizontalPadding
+        ])
+        .padding(0.5)
+        .domain(data.caseTypes.map((d) => d.name));
+
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data.caseTypes, (d) => d.totalUnits)])
+        .range([
+          chartProps.height - chartProps.verticalPadding,
+          chartProps.verticalPadding
+        ]);
+
+      const make_y_gridlines = () =>
+        d3
+          .axisLeft(yScale)
+          .ticks(4)
+          .tickFormat(function (d) {
+            return d / chartProps.unitDivider;
+          });
+
+      const chartContainer = svg.append('g');
+
+      const axis = chartContainer
+        .append('g')
+        .call(make_y_gridlines().tickSize(-chartProps.width))
+        .call((g) => {
+          g.selectAll('line').classed('axis-lines', true);
+
+          g.selectAll('text')
+            .attr('x', chartProps.axisLeftLabelPosition.x)
+            .attr('y', chartProps.axisLeftLabelPosition.y)
+            .classed('axis-labels', true);
+        })
+        .call((g) => g.select('.domain').remove());
+
+      const bars = chartContainer
+        .append('g')
+        .selectAll('rect')
+        .data(data.caseTypes)
+        .enter()
+        .append('rect')
+        .attr('x', (d) => xScale(d.name))
+        .attr('y', (d) => yScale(d.totalUnits))
+        .attr('width', xScale.bandwidth())
+        .attr(
+          'height',
+          (d) =>
+            chartProps.height -
+            chartProps.verticalPadding -
+            yScale(d.totalUnits)
+        )
+        .attr('fill', () => generateRandomColor());
+
+      const addBarLabels = ({ verticalPosition, labelText }) => {
+        chartContainer
+          .append('g')
+          .selectAll('foreignObject')
+          .data(data.caseTypes)
+          .enter()
+          .append('foreignObject')
+          .attr(
+            'x',
+            (d) =>
+              xScale(d.name) +
+              xScale.bandwidth() / 2 -
+              chartProps.labelWidth / 2
+          )
+          .attr('y', verticalPosition)
+          .attr('width', chartProps.labelWidth)
+          .attr('height', chartProps.labelHeight)
+          .append('xhtml:p')
+          .text(labelText)
+          .classed('label', true);
+      };
+
+      const upperLabel = {
+        verticalPosition: (d) =>
+          yScale(d.totalUnits) - chartProps.verticalPadding / 2,
+        labelText: (d) =>
+          `${Math.round(d.totalUnits / chartProps.unitDivider)}k Units`
+      };
+      const lowerLabel = {
+        verticalPosition: (d) => chartProps.height - chartProps.verticalPadding,
+        labelText: (d) => d.name
+      };
+
+      addBarLabels(upperLabel);
+      addBarLabels(lowerLabel);
+    }
+  }
+};
+</script>
+
+<style>
+.bar {
+  display: flex;
+  background-color: #fff;
+}
+
+.label {
+  font-size: 10px;
+  text-align: center;
+  color: black;
+}
+
+.axis-labels {
+  stroke: #aaa;
+  text-anchor: middle;
+}
+
+.axis-lines {
+  stroke: #ccc;
+  stroke-width: 0.7px;
+}
+</style>
---
+++ vue-d3/src/data/chartData.js
@@ -0,0 +1,52 @@
+const data = {
+  summary: {
+    totalCases: 500,
+    totalPOs: 55
+  },
+  caseTypes: [
+    {
+      name: 'Customer Master',
+      caseGroupId: 3,
+      totalExceptions: 23,
+      totalUnits: 1000
+    },
+    {
+      name: 'Material Master ',
+      caseGroupId: 4,
+      totalExceptions: 226,
+      totalUnits: 5992
+    },
+    {
+      name: 'Legal Statutory Compliance',
+      caseGroupId: 8,
+      totalExceptions: 2,
+      totalUnits: 8000
+    },
+    {
+      name: 'Comp Portal',
+      caseGroupId: 9,
+      totalExceptions: 5,
+      totalUnits: 1000
+    },
+    {
+      name: 'STO ',
+      caseGroupId: 10,
+      totalExceptions: 223,
+      totalUnits: 1000
+    },
+    {
+      name: 'RSTO',
+      caseGroupId: 11,
+      totalExceptions: 23,
+      totalUnits: 3680
+    },
+    {
+      name: 'RSO',
+      caseGroupId: 12,
+      totalExceptions: 18,
+      totalUnits: 1000
+    }
+  ]
+};
+
+export default data;
